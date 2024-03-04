7c7058b5cb0bf6d33fd036ac8bf2fb712d733011
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-03-04 19:05:34 +0100

    Update contactsControllers.js
    created fn getAllContacts, getOneContact

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,8 +1,28 @@
-// import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import { getContactById, listContacts } from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await listContacts();
 
-export const getOneContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await getContactById(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteContact = (req, res) => {};
 
