ca9568059392f7c6e520788d6ec5134b8761ec7c
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-03-06 12:56:20 +0100

    fix
    fixed error message for updateContactSchema

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -10,4 +10,8 @@ export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-}).min(1);
+})
+  .min(1)
+  .messages({
+    "object.min": "Body must have at least one field",
+  });
