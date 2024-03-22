33b1b0999a0cf5a073ff5629ccfd098acbb81552
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-03-22 14:55:39 +0100

    created middlewares upload

---
+++ middlewares/upload.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+import path from "path";
+import HttpError from "../helpers/HttpError.js";
+
+const destination = path.resolve("tmp");
+
+const storage = multer.diskStorage({
+  destination,
+  filename: (req, file, callback) => {
+    const uniquePrefix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const fileName = `${uniquePrefix}_${file.originalname}`;
+    callback(null, fileName);
+  },
+});
+
+const limits = {
+  fileSize: 1024 * 1024 * 5,
+};
+
+const upload = multer({
+  storage,
+  limits,
+});
+
+export default upload;
